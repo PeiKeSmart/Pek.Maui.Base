f80f5143c9381eaa403c6cc5ecde6f2c9ea0c6db
猿人易 <qcjxberin@outlook.com>
2024-09-05 12:33:15 +0800

    添加Pek命名空间及IOHelper类的ReadBytes方法
    新增Pek命名空间，并在其下添加静态类IOHelper。
    在IOHelper类中新增ReadBytes静态扩展方法，用于从源数组
    复制指定数量的字节。方法参数包括src、offset和count。
    方法逻辑包括处理count为0或负数的情况，并使用Buffer.BlockCopy
    进行字节复制。

---
+++ Pek.Maui.Base/IO/IOHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace Pek;
+
+/// <summary>IO工具类</summary>
+public static class IOHelper
+{
+    #region 复制数据流
+    /// <summary>复制数组</summary>
+    /// <param name="src">源数组</param>
+    /// <param name="offset">起始位置。一般从0开始</param>
+    /// <param name="count">复制字节数。用-1表示截取剩余所有数据</param>
+    /// <returns>返回复制的总字节数</returns>
+    public static Byte[] ReadBytes(this Byte[] src, Int32 offset, Int32 count)
+    {
+        if (count == 0) return [];
+
+        // 即使是全部，也要复制一份，而不只是返回原数组，因为可能就是为了复制数组
+        if (count < 0) count = src.Length - offset;
+
+        var bts = new Byte[count];
+        Buffer.BlockCopy(src, offset, bts, 0, bts.Length);
+        return bts;
+    }
+    #endregion
+}
