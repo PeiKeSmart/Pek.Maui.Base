3f00541f3708723714d33f66910de4fe452f52b6
猿人易 <qcjxberin@outlook.com>
2024-09-05 11:39:27 +0800

    基础项目

---
+++ Pek.Maui.Base/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace Pek.Maui.Base
+{
+    // All the code in this file is only included on Android.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ Pek.Maui.Base/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace Pek.Maui.Base
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
