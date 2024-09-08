f8d8393d26fa61e77e0a7de0f5fca91f0b7f834d
猿人易 <qcjxberin@outlook.com>
2024-09-08 12:57:02 +0800

    添加 CommunityToolkit.Maui 包并新增 MessageBox 类
    在 `Pek.Maui.Base.csproj` 文件中添加了 `CommunityToolkit.Maui` 包引用，版本为 `9.0.3`。
    
    在 `MessageBox.cs` 文件中：
    - 引入了 `CommunityToolkit.Maui.Alerts` 和 `CommunityToolkit.Maui.Core` 命名空间。
    - 创建了 `Pek.Message` 命名空间。
    - 新增了 `MessageBox` 静态类，包含以下成员：
      - `ALERT_TITLE` 属性，默认值为 "湖北登灏"。
      - `DHToast` 方法，显示一个短暂的提示信息，约2秒后消失。
      - 重载的 `DHToast` 方法，显示一个较长时间的提示信息，约3.5秒后消失。
      - `Debug` 方法，在调试控制台中编写调试消息。
      - `ShowAlert` 扩展方法，在页面上显示一个消息框，标题默认为 `ALERT_TITLE`。

---
+++ Pek.Maui.Base/Message/MessageBox.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace Pek.Message;
+
+public static class MessageBox
+{
+    public static String ALERT_TITLE { get; set; } = "湖北登灏";
+
+    /// <summary>
+    /// 弹出提示信息,约2秒左右会消失
+    /// </summary>
+    /// <param name="Message"></param>
+    /// <returns></returns>
+    public static async Task DHToast(String Message)
+    {
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        var duration = ToastDuration.Short;
+        double fontSize = 14;
+
+        var toast = Toast.Make(Message, duration, fontSize);
+
+        await toast.Show(cancellationTokenSource.Token);
+    }
+
+    /// <summary>
+    /// 弹出提示信息，约3.5秒左右会消失
+    /// </summary>
+    /// <param name="Message"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public static async Task DHToast(String Message, CancellationToken cancellationToken)
+    {
+        var duration = ToastDuration.Long;
+        double fontSize = 14;
+
+        var toast = Toast.Make(Message, duration, fontSize);
+
+        await toast.Show(cancellationToken);
+    }
+
+    /// <summary>
+    /// 在调试控制台中编写调试消息
+    /// </summary>
+    /// <param name="message">要显示的消息</param>
+    public static void Debug(string message) => System.Diagnostics.Debug.WriteLine(message);
+
+    /// <summary>
+    /// 显示消息
+    /// </summary>
+    /// <param name="message">要显示的消息</param>
+    /// <param name="title">消息标题</param>
+    /// <param name="page">页面</param>
+    /// <returns>要执行的任务</returns>
+    public static Task ShowAlert(this Page page, String message, String? title = null) => page.DisplayAlert(string.IsNullOrWhiteSpace(title) ? ALERT_TITLE : title, message, "OK");
+
+}
