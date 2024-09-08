1ba17bd7a6a5f9133a5bfe7d8b6535fd5835950e
猿人易 <qcjxberin@outlook.com>
2024-09-08 11:40:59 +0800

    新增权限工具类和服务提供者类
    在 `Pek.Maui.Base.csproj` 文件中：
      * 修改了 `<NoWarn>` 标签，新增了警告代码 `1591` 和 `CS0067`，以忽略这些特定的编译警告。
    
    在 `PermissionUtil.cs` 文件中：
      * 新增了 `PermissionUtil` 类，用于动态请求权限。
      * 该类是一个单例模式，提供了 `Instance` 属性来获取唯一实例。
      * 新增了 `CheckPermissionAndRequestPermissionAsync<T>` 方法，用于检查并请求权限。
    
    在 `ServiceProvider.cs` 文件中：
      * 新增了 `ServiceProvider` 静态类，用于获取服务。
      * 根据不同的平台（Windows、Android、iOS、MacCatalyst）获取当前的服务提供者。
      * 新增了 `GetService<T>` 方法，用于获取指定类型的服务。

---
+++ Pek.Maui.Base/PermissionUtil.cs
@@ -0,0 +1,38 @@
+﻿using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace Pek;
+
+/// <summary>
+/// 权限动态请求
+/// </summary>
+public class PermissionUtil
+{
+    private PermissionUtil() { }
+
+    static PermissionUtil? permissionUtil = null;
+
+    public static PermissionUtil Instance
+    {
+        get
+        {
+            if (permissionUtil == null)
+            {
+                permissionUtil = new PermissionUtil();
+            }
+            return permissionUtil;
+        }
+    }
+
+    public async Task<PermissionStatus> CheckPermissionAndRequestPermissionAsync<T>(T permissionReq)
+             where T : BasePermission
+    {
+        var status = await permissionReq.CheckStatusAsync();
+
+        if (status != PermissionStatus.Granted)
+        {
+            status = await permissionReq.RequestAsync();
+        }
+
+        return status;
+    }
+}
---
+++ Pek.Maui.Base/Services/ServiceProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace Pek;
+
+public static class ServiceProvider
+{
+    static IServiceProvider Current =>
+#if WINDOWS10_0_17763_0_OR_GREATER
+			MauiWinUIApplication.Current.Services;
+#elif ANDROID
+            ((IPlatformApplication)MauiApplication.Current).Services;
+#elif IOS || MACCATALYST
+            ((IPlatformApplication)MauiUIApplicationDelegate.Current).Services;
+#else
+            throw new NotImplementedException();
+#endif
+
+    public static T GetService<T>() where T : notnull
+        => Current.GetRequiredService<T>();
+}
