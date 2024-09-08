f45d799649107b8bfd7e6e4ad85c9f9dd67f6b4b
猿人易 <qcjxberin@outlook.com>
2024-09-08 12:49:24 +0800

    添加 `Pek.IO` 命名空间及 `DeviceHelper` 类
    新增 `Pek.IO` 命名空间和静态类 `DeviceHelper`。
    在 `DeviceHelper` 类中添加了静态方法 `GetPackageName`，
    用于根据编译条件获取 Android 或 iOS 应用的包名。
    
    添加 DeviceHelper 类以获取应用包名
    
    添加了一个新的命名空间 `Pek.IO`。
    在 `Pek.IO` 命名空间下新增了一个静态类 `DeviceHelper`。
    在 `DeviceHelper` 类中新增了一个静态方法 `GetPackageName`，用于获取应用程序的包名。
    `GetPackageName` 方法根据编译条件（`ANDROID` 或 `IOS`）分别获取 Android 或 iOS 应用的包名。

---
+++ Pek.Maui.Base/IO/DeviceHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Pek.IO;
+
+public static class DeviceHelper
+{
+    /// <summary>
+    /// 获取APP包名
+    /// </summary>
+    /// <returns></returns>
+    public static String? GetPackageName()
+    {
+        var packageName = String.Empty;
+
+#if ANDROID
+            packageName = Android.App.Application.Context.PackageName;
+#elif IOS
+        packageName = Foundation.NSBundle.MainBundle.BundleIdentifier;
+#endif
+
+        return packageName;
+    }
+}
