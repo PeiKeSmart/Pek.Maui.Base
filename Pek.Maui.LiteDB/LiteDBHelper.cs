8a9ba34d313e338151f7f6d4be4ad540793b14f4
猿人易 <qcjxberin@outlook.com>
2025-03-16 21:52:30 +0800

    添加 Pek.Maui.LiteDB 项目及相关功能
    在 `publish-beta.yml` 和 `publish.yml` 中添加了对 `Pek.Maui.LiteDB` 项目的构建和发布步骤，并更新了 NuGet 推送源。
    在解决方案文件中嵌套了 `Pek.Maui.LiteDB` 项目以优化结构。
    新增 `LiteDBHelper` 类，提供对 LiteDB 数据库的基本操作。
    在 `Pek.Maui.LiteDB.csproj` 中定义了 SDK 和目标框架，并添加了必要的依赖项。
    为不同平台（Android、iOS、Mac Catalyst、Tizen 和 Windows）添加了平台特定的类。

---
+++ Pek.Maui.LiteDB/LiteDBHelper.cs
@@ -0,0 +1,187 @@
+﻿using LiteDB;
+
+namespace Pek.Maui.LiteDB;
+
+public class LiteDBHelper
+{
+    /// <summary>
+    /// 获取指定路径的数据库
+    /// </summary>
+    /// <param name="liteDbPath"></param>
+    /// <returns></returns>
+    public static LiteDatabase GetDatabase(String liteDbPath)
+    {
+        LiteDatabase? liteDatabaseCurrent = null;
+
+        using (var liteDatabase = new LiteDatabase(liteDbPath))
+        {
+            liteDatabaseCurrent = liteDatabase;
+        }
+
+        return liteDatabaseCurrent;
+    }
+
+    /// <summary>
+    /// 清空表
+    /// </summary>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <returns></returns>
+    public static Boolean TruncateCollection(String liteDbPath, String liteDbCollectionName)
+    {
+        var liteDatabase = GetDatabase(liteDbPath);
+
+        return liteDatabase.DropCollection(liteDbCollectionName);
+    }
+
+    /// <summary>
+    /// 获取集合
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <returns></returns>
+    public static ILiteCollection<T> GetCollection<T>(String liteDbPath, String liteDbCollectionName)
+    {
+        ILiteCollection<T>? liteCollection = null;
+
+        using (var liteDatabase = new LiteDatabase(liteDbPath))
+        {
+            liteCollection = liteDatabase.GetCollection<T>(liteDbCollectionName);
+        }
+
+        return liteCollection;
+    }
+
+    /// <summary>
+    /// 获取全部集合
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <returns></returns>
+    public static IEnumerable<T> FindAll<T>(String liteDbPath, String liteDbCollectionName)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        return liteCollection.FindAll();
+    }
+
+    /// <summary>
+    /// 获取实体
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDatabasePath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public static IEnumerable<T> Find<T>(String liteDatabasePath, String liteDbCollectionName, System.Linq.Expressions.Expression<Func<T, Boolean>> predicate)
+    {
+        var liteCollection = GetCollection<T>(liteDatabasePath, liteDbCollectionName);
+        return liteCollection.Find(predicate);
+    }
+
+    /// <summary>
+    /// 获取实体
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <param name="liteDBQuery"></param>
+    /// <returns></returns>
+    public static IEnumerable<T> Find<T>(String liteDbPath, String liteDbCollectionName, Query liteDBQuery)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        return liteCollection.Find(liteDBQuery);
+    }
+
+    /// <summary>
+    /// 根据指定Id获取数据
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public static T FindById<T>(String liteDbPath, String liteDbCollectionName, Int32 id)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        return liteCollection.FindById(new BsonValue(id));
+    }
+
+    /// <summary>
+    /// 插入
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static Boolean Insert<T>(String liteDbPath, String liteDbCollectionName, T item)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        var bsonValueInsertResult = liteCollection.Insert(item);
+        return bsonValueInsertResult != null;
+    }
+
+    /// <summary>
+    /// 插入
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <param name="items"></param>
+    /// <returns></returns>
+    public static Boolean Insert<T>(String liteDbPath, String liteDbCollectionName, IEnumerable<T> items)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        BsonValue? bsonValueInsertResult = liteCollection.Insert(items);
+        return bsonValueInsertResult != null;
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static Boolean Update<T>(String liteDbPath, String liteDbCollectionName, T item)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        BsonValue? bsonValueUpdateResult = liteCollection.Update(item);
+        return bsonValueUpdateResult != null;
+    }
+
+    /// <summary>
+    /// 删除
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="liteDbPath"></param>
+    /// <param name="liteDbCollectionName"></param>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public static Boolean Delete<T>(String liteDbPath, String liteDbCollectionName, Int32 id)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        return liteCollection.Delete(id);
+    }
+
+    public static Int32 Delete<T>(String liteDbPath, String liteDbCollectionName, System.Linq.Expressions.Expression<Func<T, Boolean>> predicate)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        return liteCollection.DeleteMany(predicate);
+    }
+
+    public static Boolean Exist<T>(String liteDbPath, String liteDbCollectionName, System.Linq.Expressions.Expression<Func<T, Boolean>> predicate)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        return liteCollection.Exists(predicate);
+    }
+
+    public static Boolean Exist<T>(String liteDbPath, String liteDbCollectionName, Query liteDBQuery)
+    {
+        var liteCollection = GetCollection<T>(liteDbPath, liteDbCollectionName);
+        return liteCollection.Exists(liteDBQuery);
+    }
+}
